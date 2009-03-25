64363f45112c200ade9e1ac524f6bde012ab1fa5
Hendrik Mans <hendrik@mans.de>
2009-03-25 16:44:25 +0100

    Add default scope to Pair model to order by time of creation by default.

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,6 +1,8 @@
 class Pair < ActiveRecord::Base
   validates_presence_of :item_1, :item_2
   belongs_to :creator, :class_name => 'User'   # can be nil!
+
+  default_scope :order => 'created_at DESC'
   
   def items
     [self.item_1, self.item_2]
