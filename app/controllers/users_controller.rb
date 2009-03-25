684e3eb7003027d073682afad3e1fc859e31f57b
Hendrik Mans <hendrik@mans.de>
2009-03-25 18:54:42 +0100

    Display user profile page. #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all(:order => "created_at DESC")
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
   
 end
