e90284d5d7d96c655331e17d8b89fb09b5e1b693
Hendrik Mans <hendrik@mans.de>
2009-03-26 16:20:27 +0100

    Hot damn, default_scope was messing up named_scopes! I'm shocked and appalled!

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -3,13 +3,15 @@ class Pair < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User'   # can be nil!
   has_many :choices, :dependent => :destroy
 
-  default_scope :order => 'created_at DESC'
-  
   def items
     [self.item_1, self.item_2]
   end
   
   def items=(list)
     self.item_1, self.item_2 = list
+  end
+  
+  def self.find_next_for(user)
+    self.random.find(:first)  # XXX
   end
 end
