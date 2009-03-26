67a988f47228c303419b256d04676c4a4564177d
Hendrik Mans <hendrik@mans.de>
2009-03-26 13:04:50 +0100

    New users can sign up. #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to Rock Or Pop!"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
   def index
     @users = User.all(:order => "created_at DESC")
   end
