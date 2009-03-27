3dc471a409b489a09ebe5205aa59e2ca3fa6920c
Hendrik Mans <hendrik@mans.de>
2009-03-27 13:02:06 +0100

    Fix Pair appearing SNAFU. I'm a fool! A foolish fool!

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -16,7 +16,7 @@ class PairsController < ApplicationController
       @choice = Choice.find_or_initialize_by_user_id_and_pair_id(User.current.id, @pair.id)  # XXX: hardcoded user ID, nasty!
       @choice.item = params[:item]
       @choice.save!
-      session[:pair_ids].delete(@choice.id)
+      session[:pair_ids].delete(@pair.id)
     
       if @next_pair = Pair.unanswered_by(User.current).random.first(:conditions => ["pairs.id NOT IN (?)", session[:pair_ids]])
         session[:pair_ids] << @next_pair.id
